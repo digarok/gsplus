56b204f052f7f6d02828e83d81c0277f48e9514b
Dagen Brock <dagenbrock@gmail.com>
2017-01-23 15:00:06 -0600

    trying to include config in mac bundle

--- make_dist_mac.sh
+++ make_dist_mac.sh
@@ -5,6 +5,7 @@ mkdir -p $DDIR/Contents/MacOS
 mkdir -p $DDIR/Contents/Resources
 
 cp gsplus $DDIR/Contents/MacOS
+cp config.txt $DDIR/Contents/MacOS
 cp $ADIR/Info.plist $DDIR/Contents
 cp $ADIR/gsp-icons.icns $DDIR/Contents/Resources
 dylibbundler -od -b -x $DDIR/Contents/MacOS/gsplus -d $DDIR/Contents/libs/
@@ -14,7 +15,5 @@ cp doc/gsplusmanual.pdf $DEXTRAS
 cp doc/gsplusmanual.txt $DEXTRAS
 cp doc/README.txt $DEXTRAS
 cp COPYING.txt $DEXTRAS
-
-#tar -cvzf gsplus-osx.tar.gz gsplus-osx/
 
 # packaging now in DMG script
