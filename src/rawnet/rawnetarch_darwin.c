698678f508ad29b85a2619e63c94ec0b003aebd9
Kelvin Sherlock <ksherlock@gmail.com>
2018-12-13 23:20:04 -0500

    rawnet
    vice cleaned up TFE and called it RAWNET.  Slightly better code separation.
    I removed libnet stuff in the unix version and added a darwin version that uses the vmnet framework.

---
+++ src/rawnet/rawnetarch_darwin.c
@@ -0,0 +1,255 @@
+/*
+ * OS X 10.10+
+ * vmnet support (clang -framework vmnet -framework Foundation)
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <vmnet/vmnet.h>
+
+#include "rawnetarch.h"
+#include "rawnetsupp.h"
+
+
+static const uint8_t broadcast_mac[6] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+
+static interface_ref interface;
+static uint8_t interface_mac[6];
+static uint64_t interface_mtu;
+static uint64_t interface_packet_size;
+static vmnet_return_t interface_status;
+
+int rawnet_arch_init(void) {
+	interface = NULL;
+	return 1;
+}
+void rawnet_arch_pre_reset(void) {
+	/* NOP */
+}
+
+void rawnet_arch_post_reset(void) {
+	/* NOP */
+}
+
+int rawnet_arch_activate(const char *interface_name) {
+
+	xpc_object_t dict;
+	dispatch_queue_t q;
+	dispatch_semaphore_t sem;
+
+	/*
+	 * there's no way to set the MAC address directly.
+	 * using vmnet_interface_id_key w/ the previous interface id
+	 * *MIGHT* re-use the previous MAC address.
+	 */
+
+	memset(interface_mac, 0, sizeof(interface_mac));
+	interface_status = 0;
+	interface_mtu = 0;
+	interface_packet_size = 0;
+
+	dict = xpc_dictionary_create(NULL, NULL, 0);
+	xpc_dictionary_set_uint64(dict, vmnet_operation_mode_key, VMNET_SHARED_MODE);
+	sem = dispatch_semaphore_create(0);
+	q = dispatch_get_global_queue(QOS_CLASS_UTILITY, 0);
+
+	interface = vmnet_start_interface(dict, q, ^(vmnet_return_t status, xpc_object_t params){
+		interface_status = status;
+		if (status == VMNET_SUCCESS) {
+			const char *cp;
+			cp = xpc_dictionary_get_string(params, vmnet_mac_address_key);
+			sscanf(cp, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+				&interface_mac[0],
+				&interface_mac[1],
+				&interface_mac[2],
+				&interface_mac[3],
+				&interface_mac[4],
+				&interface_mac[5]
+			);
+
+			interface_mtu = xpc_dictionary_get_uint64(params, vmnet_mtu_key);
+			interface_packet_size =  xpc_dictionary_get_uint64(params, vmnet_max_packet_size_key);
+		}
+		dispatch_semaphore_signal(sem);
+	});
+	dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+
+
+	if (interface_status != VMNET_SUCCESS) {
+		log_message(rawnet_arch_log, "vmnet_start_interface failed\n");
+		if (interface) {
+			vmnet_stop_interface(interface, q, ^(vmnet_return_t status){
+				dispatch_semaphore_signal(sem);
+			});
+			dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+			interface = NULL;
+		}
+	}
+
+
+	dispatch_release(sem);
+	xpc_release(dict);
+	return interface_status == VMNET_SUCCESS;
+}
+
+void rawnet_arch_deactivate(void) {
+
+	dispatch_queue_t q;
+	dispatch_semaphore_t sem;
+
+
+	if (interface) {
+		sem = dispatch_semaphore_create(0);
+		q = dispatch_get_global_queue(QOS_CLASS_UTILITY, 0);
+
+		vmnet_stop_interface(interface, q, ^(vmnet_return_t status){
+			dispatch_semaphore_signal(sem);
+		});
+		dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
+		dispatch_release(sem);
+
+		interface = NULL;
+		interface_status = 0;
+	}
+
+}
+
+void rawnet_arch_set_mac(const uint8_t mac[6]) {
+	/* NOP */
+}
+void rawnet_arch_set_hashfilter(const uint32_t hash_mask[2]) {
+	/* NOP */
+}
+
+void rawnet_arch_recv_ctl(int bBroadcast, int bIA, int bMulticast, int bCorrect, int bPromiscuous, int bIAHash) {
+	/* NOP */
+}
+
+void rawnet_arch_line_ctl(int bEnableTransmitter, int bEnableReceiver) {
+	/* NOP */
+}
+
+void rawnet_arch_transmit(int force, int onecoll, int inhibit_crc, int tx_pad_dis, int txlength, uint8_t *txframe) {
+
+	int count = 1;
+	vmnet_return_t st;
+	struct vmpktdesc v;
+	struct iovec iov;
+
+	if (txlength == 0) return;
+	if (txlength > interface_packet_size) {
+		log_message(rawnet_arch_log, "packet is too big: %d\n", txlength);
+		return;
+	}
+
+	iov.iov_base = txframe;
+	iov.iov_len = txlength;
+
+	v.vm_pkt_size = txlength;
+	v.vm_pkt_iov = &iov;
+	v.vm_pkt_iovcnt = 1;
+	v.vm_flags = 0;
+	
+	st = vmnet_write(interface, &v, &count);
+	if (st != VMNET_SUCCESS) {
+		log_message(rawnet_arch_log, "vmnet_write failed!\n");
+	}
+	return;
+}
+
+int rawnet_arch_receive(uint8_t *pbuffer, int *plen, int *phashed, int *phash_index, int *prx_ok, int *pcorrect_mac, int *pbroadcast, int *pcrc_error) {
+
+	char *buffer;
+	//unsigned hashreg;
+
+	int count = 1;
+	int xfer;
+	vmnet_return_t st;
+	struct vmpktdesc v;
+	struct iovec iov;
+
+
+	buffer = malloc(interface_packet_size);
+
+	iov.iov_base = buffer;
+	iov.iov_len = interface_packet_size;
+
+	v.vm_pkt_size = interface_packet_size;
+	v.vm_pkt_iov = &iov;
+	v.vm_pkt_iovcnt = 1;
+	v.vm_flags = 0;
+
+	st = vmnet_read(interface, &v, &count);
+	if (st != VMNET_SUCCESS) {
+		log_message(rawnet_arch_log, "vmnet_write failed!\n");
+		free(buffer);
+		return 0;
+	}
+
+	if (count == 0) {
+		free(buffer);
+		return 0;
+	}
+	xfer = iov.iov_len;
+	if (xfer > *plen) xfer = *plen;
+	memcpy(pbuffer, buffer, xfer);
+
+	xfer = iov.iov_len;
+	if (xfer & 0x01) ++xfer; /* ??? */
+	*plen = xfer; /* actual frame size */
+
+	*phashed =
+	*phash_index =
+	*pbroadcast = 
+	*pcorrect_mac =
+	*pcrc_error = 0;
+
+	*prx_ok = 1;
+	*pcorrect_mac = memcmp(buffer, interface_mac, 6) == 0;
+	*pbroadcast = memcmp(buffer, broadcast_mac, 6) == 0;
+	
+	/* vmnet won't send promiscuous packets */
+	#if 0
+	hashreg = (~crc32_buf(buffer, 6) >> 26) & 0x3f;
+	if (hash_mask[hashreg >= 32] & (1 << (hashreg & 0x1F))) {
+		*phashed = 1;
+		*phash_index = hashreg;
+	} else {
+		*phashed = 0;
+		*phash_index = 0;
+	}
+	#endif
+
+	free(buffer);
+	return 1;
+}
+
+static unsigned adapter_index = 0;
+int rawnet_arch_enumadapter_open(void) {
+	adapter_index = 0;
+	return 1;
+}
+
+int rawnet_arch_enumadapter(char **ppname, char **ppdescription) {
+
+	if (adapter_index == 0) {
+		++adapter_index;
+		if (ppname) *ppname = lib_stralloc("vmnet");
+		if (ppdescription) *ppdescription = lib_stralloc("vmnet");
+		return 1;
+	}
+	return 0;
+}
+
+int rawnet_arch_enumadapter_close(void) {
+	return 1;
+}
+
+char *rawnet_arch_get_standard_interface(void) {
+	return lib_stralloc("vmnet");
+}
---
+++ src/rawnet/rawnetsupp.c
@@ -0,0 +1,151 @@
+/*
+ * This file is a consolidation of functions required for tfe
+ * emulation taken from the following files
+ *
+ * lib.c   - Library functions.
+ * util.c  - Miscellaneous utility functions.
+ * crc32.c
+ *
+ * Written by
+ *  Andreas Boose <viceteam@t-online.de>
+ *  Ettore Perazzoli <ettore@comm2000.it>
+ *  Andreas Matthies <andreas.matthies@gmx.net>
+ *  Tibor Biczo <crown@mail.matav.hu>
+ *  Spiro Trikaliotis <Spiro.Trikaliotis@gmx.de>*
+ *
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "rawnetsupp.h"
+
+
+
+#define CRC32_POLY  0xedb88320
+static unsigned long crc32_table[256];
+static int crc32_is_initialized = 0;
+
+void lib_free(void *ptr) {
+  free(ptr);
+}
+
+void *lib_malloc(size_t size) {
+
+  void *ptr = malloc(size);
+
+  if (ptr == NULL && size > 0) {
+    perror("lib_malloc");
+    exit(-1);
+  }
+
+  return ptr;
+}
+
+/*-----------------------------------------------------------------------*/
+
+/* Malloc enough space for `str', copy `str' into it and return its
+   address.  */
+char *lib_stralloc(const char *str) {
+  char *ptr;
+
+  if (str == NULL) {
+    fprintf(stderr, "lib_stralloc\n");
+    exit(-1);
+  }
+
+  ptr = strdup(str);
+  if (!ptr) {
+    perror("lib_stralloc");
+    exit(-1);
+  }
+
+  return ptr;
+}
+
+
+
+/* Like realloc, but abort if not enough memory is available.  */
+void *lib_realloc(void *ptr, size_t size) {
+
+
+  void *new_ptr = realloc(ptr, size);
+
+  if (new_ptr == NULL) {
+    perror("lib_realloc");
+    exit(-1);
+  }
+
+  return new_ptr;
+}
+
+// Util Stuff
+
+/* Set a new value to the dynamically allocated string *str.
+   Returns `-1' if nothing has to be done.  */
+int util_string_set(char **str, const char *new_value) {
+  if (*str == NULL) {
+    if (new_value != NULL)
+      *str = lib_stralloc(new_value);
+  } else {
+    if (new_value == NULL) {
+      lib_free(*str);
+      *str = NULL;
+    } else {
+      /* Skip copy if src and dest are already the same.  */
+      if (strcmp(*str, new_value) == 0)
+        return -1;
+
+      *str = (char *)lib_realloc(*str, strlen(new_value) + 1);
+      strcpy(*str, new_value);
+    }
+  }
+  return 0;
+}
+
+
+// crc32 Stuff
+
+unsigned long crc32_buf(const char *buffer, unsigned int len) {
+  int i, j;
+  unsigned long crc, c;
+  const char *p;
+
+  if (!crc32_is_initialized) {
+    for (i = 0; i < 256; i++) {
+      c = (unsigned long) i;
+      for (j = 0; j < 8; j++)
+        c = c & 1 ? CRC32_POLY ^ (c >> 1) : c >> 1;
+      crc32_table[i] = c;
+    }
+    crc32_is_initialized = 1;
+  }
+
+  crc = 0xffffffff;
+  for (p = buffer; len > 0; ++p, --len)
+    crc = (crc >> 8) ^ crc32_table[(crc ^ *p) & 0xff];
+
+  return ~crc;
+}
